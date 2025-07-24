d8d05c9b2e7abad01c6de517e222c9f9e3bd1e2a
thibovg1 <thibo.vangestel@gmail.com>
2025-07-24 11:35:01 +0200

    GMS V2

--- index_script.js
+++
@@ -1,5 +0,0 @@
-// index_script.js (was welcome_script.js inhoud)
-// Dit script beheert de welkomstpagina, die mogelijk geen complexe JS nodig heeft.
-document.addEventListener('DOMContentLoaded', () => {
-    // Eventueel toekomstige JavaScript-functionaliteit voor de welkomstpagina.
-});
